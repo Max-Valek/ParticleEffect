a2a3823999b30b0e80f6051dd964b75433fe3c82
Max Valek <valek.maxwell@gmail.com>
2023-04-28 13:38:03 -0500

    Initial Commit

---
+++ ParticleEffect/ParticleEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleEffectApp.swift
+//  ParticleEffect
+//
+//  Created by Max Valek on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ParticleEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
